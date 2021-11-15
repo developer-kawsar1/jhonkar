28fe274208a3a1ced2bceaaf687b9e5bca8a1db5
kawsar30 <zislam229@gmail.com>
2021-11-15 14:00:04 +0600

    our first website

---
+++ footer.php
@@ -0,0 +1,25 @@
+ <!-- footer part -->
+    <footer id="footer">
+        <div class="container">
+            <div class="my-row">
+                <div class="colm-12">
+                    <div class="footer-part text-center">
+                        <div class="footer-logo">
+                            <img src="<?php echo get_template_directory_uri();   ?>/img/Logo.png" alt="">
+                        </div>
+                        <div class="social-icon">
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-dribbble"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-youtube"></i></a>
+                        </div>
+                        <div class="copy-text">
+                            <p>Copyright &copy; 2022 Football</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
